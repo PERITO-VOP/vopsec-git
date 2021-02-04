eee1c664d6d0ac8741d4332b1bc19932012c0c66
VOP <vop88@gmx.com>
2021-02-04 16:09:18 -0300

    Alterao informando uso PHP

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <p>Página do Projeto N4rt2r</p>
         <p>este é o segundo parágrafo</p>
         <p>Este é o terceiro parágrafo</p>
+        <p>Este parágrafo foi adicionado após um commit no Git</p>
         <p> </p>
         <?php 
             echo "<strong>Hallo Welt</strong>";
