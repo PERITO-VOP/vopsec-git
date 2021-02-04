dde53572949b8aa43997b2cae15d261f8340fdbe
VOP <vop88@gmx.com>
2021-02-04 15:49:38 -0300

    Arquivo de teste com PHP adicionado com git commit -m <mensagem>

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title>Estudando Git</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <!-- Isto é um comentário -->
+        <p>Página do Projeto N4rt2r</p>
+        <p>este é o segundo parágrafo</p>
+        <p>Este é o terceiro parágrafo</p>
+        <p> </p>
+        <?php 
+            echo "<strong>Hallo Welt</strong>";
+            echo " - Esta linha foi escrita em PHP";
+        ?>
+    </body>
+</html>
